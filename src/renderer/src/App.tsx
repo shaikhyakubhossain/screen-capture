60f56961d48f0615ef3ba42cbef55f10df937802
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-18 02:08:56 +0530

    Add tailwindcss package, add tasks.json in .vscode

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,9 @@
+import './index.css'
+
 function App(): JSX.Element {
   return (
     <div>
-      <div>Hello World</div>
+      <div className='bg-gray-600' onClick={() => alert('click')}>click</div>
     </div>
   )
 }
